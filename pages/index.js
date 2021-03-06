69962a612120a3d709317b4362149f5139a97247
jonheslop <jon.heslop@pusher.com>
2021-03-06 23:30:41 +0000

    Betta meta

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,19 @@ export default function Home() {
   return (
     <div className="flex flex-col items-start justify-start min-h-screen py-2">
       <Head>
-        <title>Cassie Leavers - cass.works</title>
-
+        <title>Cassie Leavers is cass.works</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta
           name="description"
           content="Cassie Leavers is a graphic designer based in London."
         />
+        <meta property="og:url" content="https://cass.works/" />
+        <meta property="og:title" content="Cassie Leavers is cass.works" />
+        <meta
+          property="og:description"
+          content="Cassie Leavers is a graphic designer based in London."
+        />
+        <meta property="og:type" content="website" />
       </Head>
 
       <main className="flex flex-col items-start justify-start flex-1 p-8 lg:p-20">
