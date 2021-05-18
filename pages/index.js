08c9ccaa63bdad48a543743a9bbbb78d6054e835
jonheslop <jon.heslop@pusher.com>
2021-05-18 23:51:35 +0100

    Very WIPy get projects from Dato MVP

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,51 @@
 import Head from "next/head";
+import { request } from "../lib/datocms";
+import { renderMetaTags, useQuerySubscription } from "react-datocms";
+import { metaTagsFragment, responsiveImageFragment } from "../lib/fragments";
+import { Image } from "react-datocms";
+import Link from "next/link";
 
-export default function Home() {
+export async function getStaticProps({ preview }) {
+  const graphqlRequest = {
+    query: `
+      {
+        allProjects {
+          id
+          title
+          slug
+          coverImage {
+            responsiveImage(imgixParams: {fm: jpg, fit: crop, w: 600, h: 400 }) {
+              ...responsiveImageFragment
+            }
+          }
+        }
+      }
+      ${responsiveImageFragment}
+    `,
+    preview,
+  };
+
+  return {
+    props: {
+      subscription: preview
+        ? {
+            ...graphqlRequest,
+            initialData: await request(graphqlRequest),
+            token: process.env.CMS_DATOCMS_API_TOKEN,
+          }
+        : {
+            enabled: false,
+            initialData: await request(graphqlRequest),
+          },
+    },
+  };
+}
+
+export default function Home({ subscription }) {
+  const {
+    data: { allProjects },
+  } = useQuerySubscription(subscription);
+
   return (
     <div className="flex flex-col items-start justify-start min-h-screen py-2 dark:text-white dark:bg-black">
       <Head>
@@ -31,6 +76,24 @@ export default function Home() {
           <br />
           <span className="opacity-0">cass.works</span>/for-you
         </h1>
+
+        <h2 className="mt-16 text-2xl font font-medium">Recent work</h2>
+        <ul className="grid grid-cols-3 grid w-full my-16 gap-8">
+          {allProjects.map((project) => (
+            <Link as={`/projects/${project.slug}`} href="/projects/[slug]">
+              <a className="hover:underline" key={project.id}>
+                <Image
+                  data={{
+                    ...project.coverImage.responsiveImage,
+                    alt: ``,
+                  }}
+                  className="shadow-small"
+                />
+                <h3 className="mt-4">{project.title}</h3>
+              </a>
+            </Link>
+          ))}
+        </ul>
       </main>
     </div>
   );
