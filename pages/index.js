bbd8ed37223884ab90315a0e9c84c1424400ed00
Jon Heslop <jon@jonheslop.com>
2021-08-14 07:55:30 +0100

    Tweak copy

--- pages/index.js
+++ pages/index.js
@@ -3,20 +3,17 @@ import Layout from "@/components/layout";
 export default function Home() {
   return (
     <Layout title="Hello" showHomeLink={false}>
-      <h1 className="text-2xl md:text-4xl lg:text-6xl xl:text-8xl font-light">
-        cass.works/<span class="opacity-50">freelance</span>
+      <h1 className="text-2xl md:text-4xl lg:text-6xl xl:text-8xl 2xl:text-9xl font-light">
+        cass.works<span class="opacity-50">/with-you</span>
         <br />
         <span className="opacity-0">cass.works</span>
         <span class="opacity-50">/on-branding</span>
         <br />
         <span className="opacity-0">cass.works</span>
-        <span class="opacity-50">/on-environmental</span>
+        <span class="opacity-50 truncate">/on-environmental</span>
         <br />
         <span className="opacity-0">cass.works</span>
         <span class="opacity-50">/on-wayfinding</span>
-        <br />
-        <span className="opacity-0">cass.works</span>
-        <span class="opacity-50">/with-you</span>
       </h1>
     </Layout>
   );
