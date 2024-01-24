859fb9d8681001626ad8dba6c3891ec0a3ec348f
Jon Heslop <jon@jonheslop.com>
2024-01-24 15:20:23 +0000

    Remove hover line

--- pages/index.js
+++ pages/index.js
@@ -67,8 +67,8 @@ export default function Home({ subscription }) {
                 }}
                 className="shadow-small"
               />
-              <div className="mt-4 flex flex-col gap-2">
-                <h3 className="mr-8 inline-block border-b border-transparent text-2xl font-thin tracking-wide group-hover:border-white/75">
+              <div className="mt-4 flex flex-col gap-3">
+                <h3 className="mr-8 inline-block text-2xl font-thin tracking-wide">
                   {project.title}
                 </h3>
                 <div className="flex flex-wrap gap-2">
